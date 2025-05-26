568bc5c88158d36a4babed4472fa700b6fd898bd
Isak Grönlund <135023164+Grizak@users.noreply.github.com>
2025-05-26 07:07:31 +0000

    Initial commit

---
+++ client/build/sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = "ToDoListApp_v1";
+const urlsToCache = ["favicon.ico", "robots.txt"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      caches.match(event.request).then((response) => response)
+    )
+  );
+});
